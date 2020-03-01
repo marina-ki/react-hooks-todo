1fb41fd668bd14eae296270eeab60302058585bd
k-810 <k810marina@gmail.com>
2020-03-01 17:25:31 +0900

    スタイル修正

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -5,7 +5,7 @@ import AppContext from '../contexts/AppContext'
 const TodoFormBox = () => {
   const {dispatch} = useContext(AppContext);
   const [title, setTitle] = useState('')
-  const [openForm, setOpenForm] = useState(false);
+  const [openForm, setOpenForm] = useState(true);
   const addList = e => {
     e.preventDefault()
     dispatch({
@@ -25,7 +25,7 @@ const TodoFormBox = () => {
         <form>
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title"><input className="form-control" id="formEventTitle" placeholder="タイトル"  value={title} onChange={e => setTitle(e.target.value)}/></h5>
+              <h5 className="card-title"><input className="form-control" id="formEventTitle" placeholder="例：今日やることリスト" value={title} onChange={e => setTitle(e.target.value)}/></h5>
               <button className="btn btn-primary" onClick={addList} disabled={unCreatable}>作成する</button>
             </div>
           </div>
